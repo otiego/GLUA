9b6817ceaa3ddf98e7a98072c5cab04c8c29db73
Ronnie <ronald.otiego@gmail.com>
2024-01-17 12:19:42 +0300

    Second commit

---
+++ OpenvirtualEnv.bat
@@ -0,0 +1,4 @@
+@echo off
+cmd /k "cd /d C:\GLUA\DjangoInventory\.\scripts & activate & cd /d C:\GLUA\DjangoInventory\Glua & python manage.py runserver"  
+
+
---
+++ bat Edge.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+start cmd.exe /C "python manage.py runserver & cd C:\Users\henry\Desktop\DjangoInventory\Glua\ & C:"
+start C:\"Program Files (x86)"\Microsoft\Edge\Application\msedge.exe "http://127.0.0.1:8000/"
+
